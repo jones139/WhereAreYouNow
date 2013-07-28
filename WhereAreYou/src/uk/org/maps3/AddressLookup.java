2b13f0e0f82f53742d7f8b66e97e119879c2a7ca
Graham Jones <grahamjones139@gmail.com>
2013-07-28 20:41:57 +0100

    Corrected indenting to make code easier to read.

--- WhereAreYou/src/uk/org/maps3/AddressLookup.java
+++ WhereAreYou/src/uk/org/maps3/AddressLookup.java
@@ -19,10 +19,10 @@ import android.util.Log;
 
 
 interface AddressReceiver {
-	/** The function to be called once we have found the location */
-	public void onAddressFound(LonLat ll, String addressStr);
-	/** A function to be called with debug messages */
-	public void msgBox(String msg);
+    /** The function to be called once we have found the location */
+    public void onAddressFound(LonLat ll, String addressStr);
+    /** A function to be called with debug messages */
+    public void msgBox(String msg);
 }
 
 /**
@@ -30,91 +30,91 @@ interface AddressReceiver {
  *
  */
 public class AddressLookup implements Runnable {
-	public String resultStr;
-	LonLat ll;
-	AddressReceiver ar;
+    public String resultStr;
+    LonLat ll;
+    AddressReceiver ar;
+    
+    public AddressLookup(AddressReceiver ar) {
+	this.ar = ar;
+    }
+    
+    public void doLookup(LonLat ll) {
+	Log.d("AddressLookup","doLookup()");
+	this.ll = ll;
+	Thread trd = new Thread(this);
+	trd.run();
+    }
+    
+    public void run() {
+	if (ll!=null) {
+	    String url = "http://nominatim.openstreetmap.org/reverse?format=xml&" +
+		"lon="+ll.lon()+"&lat="+ll.lat();
+	    
+	    HttpGet request = new HttpGet(url);
+	    ResponseHandler<String> responseHandler =
+		new BasicResponseHandler();
+	    HttpClient client = new DefaultHttpClient();
+	    String responseBody = null;
+	    try {
+		responseBody = client.execute(request,responseHandler);
 		
-	public AddressLookup(AddressReceiver ar) {
-		this.ar = ar;
-	}
-	
-	public void doLookup(LonLat ll) {
-		Log.d("AddressLookup","doLookup()");
-		this.ll = ll;
-		Thread trd = new Thread(this);
-		trd.run();
-	}
-	
-		public void run() {
-		if (ll!=null) {
-			String url = "http://nominatim.openstreetmap.org/reverse?format=xml&" +
-					"lon="+ll.lon()+"&lat="+ll.lat();
-			
-			HttpGet request = new HttpGet(url);
-			ResponseHandler<String> responseHandler =
-					new BasicResponseHandler();
-			HttpClient client = new DefaultHttpClient();
-			String responseBody = null;
-			try {
-				responseBody = client.execute(request,responseHandler);
-			
-				if (responseBody != null && responseBody.length() >0) {
-					Log.d("AddressLookup","responseBody="+responseBody);
-				
-					XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-					factory.setNamespaceAware(true);
-					XmlPullParser xpp = factory.newPullParser();
-	
-					xpp.setInput( new StringReader ( responseBody ) );
-					int eventType = xpp.getEventType();
-					Boolean foundResult = false;
-					resultStr = null;
-					while (eventType != XmlPullParser.END_DOCUMENT) {
-						if(eventType == XmlPullParser.START_DOCUMENT) {
-							Log.d("AddressLookup","Start document");
-						} else if(eventType == XmlPullParser.START_TAG) {
-							if (xpp.getName().equals("result")) 
-								foundResult = true;
-							//Log.d("AddressLookup","Start tag "+xpp.getName());
-						} else if(eventType == XmlPullParser.END_TAG) {
-							if (xpp.getName().equals("result")) 
-								foundResult = false;
-							//Log.d("AddressLookup","End tag "+xpp.getName());
-						} else if(eventType == XmlPullParser.TEXT) {
-							if (foundResult) {
-								resultStr = xpp.getText();
-								Log.d("AddressLookup","result: "+xpp.getText());
-							}
-						}
-						eventType = xpp.next();
-					}
-					Log.d("AddressLookup","End document");
-					Log.d("AddressLookup","resultStr="+resultStr);
-				
-					ar.onAddressFound(this.ll,resultStr);
-				} else
-					ar.onAddressFound(this.ll,"Error - no response");
-			
-			} catch (ClientProtocolException e) {
-				// TODO Auto-generated catch block
-				Log.d("AddressLookup","ClientProtocolException");
-				e.printStackTrace();
-				ar.onAddressFound( this.ll,"Error - ClientProtocolException");
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				Log.d("AddressLookup","IOException");
-				e.printStackTrace();
-				ar.onAddressFound(this.ll,"Error - IOException");
-			} catch (XmlPullParserException e) {
-				// TODO Auto-generated catch block
-				Log.d("AddressLookup","XmlPullParserException");
-				e.printStackTrace();
-				ar.onAddressFound(this.ll,"Error - XmlPullParserException");
+		if (responseBody != null && responseBody.length() >0) {
+		    Log.d("AddressLookup","responseBody="+responseBody);
+		    
+		    XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+		    factory.setNamespaceAware(true);
+		    XmlPullParser xpp = factory.newPullParser();
+		    
+		    xpp.setInput( new StringReader ( responseBody ) );
+		    int eventType = xpp.getEventType();
+		    Boolean foundResult = false;
+		    resultStr = null;
+		    while (eventType != XmlPullParser.END_DOCUMENT) {
+			if(eventType == XmlPullParser.START_DOCUMENT) {
+			    Log.d("AddressLookup","Start document");
+			} else if(eventType == XmlPullParser.START_TAG) {
+			    if (xpp.getName().equals("result")) 
+				foundResult = true;
+			    //Log.d("AddressLookup","Start tag "+xpp.getName());
+			} else if(eventType == XmlPullParser.END_TAG) {
+			    if (xpp.getName().equals("result")) 
+				foundResult = false;
+			    //Log.d("AddressLookup","End tag "+xpp.getName());
+			} else if(eventType == XmlPullParser.TEXT) {
+			    if (foundResult) {
+				resultStr = xpp.getText();
+				Log.d("AddressLookup","result: "+xpp.getText());
+			    }
 			}
-		}
-		else {
-			Log.d("AddressLookup","ll is null - ignoring");
-			ar.onAddressFound(this.ll,"Error - ll is null - ignoring");
-		}
+			eventType = xpp.next();
+		    }
+		    Log.d("AddressLookup","End document");
+		    Log.d("AddressLookup","resultStr="+resultStr);
+		    
+		    ar.onAddressFound(this.ll,resultStr);
+		} else
+		    ar.onAddressFound(this.ll,"Error - no response");
+		
+	    } catch (ClientProtocolException e) {
+		// TODO Auto-generated catch block
+		Log.d("AddressLookup","ClientProtocolException");
+		e.printStackTrace();
+		ar.onAddressFound( this.ll,"Error - ClientProtocolException");
+	    } catch (IOException e) {
+		// TODO Auto-generated catch block
+		Log.d("AddressLookup","IOException");
+		e.printStackTrace();
+		ar.onAddressFound(this.ll,"Error - IOException");
+	    } catch (XmlPullParserException e) {
+		// TODO Auto-generated catch block
+		Log.d("AddressLookup","XmlPullParserException");
+		e.printStackTrace();
+		ar.onAddressFound(this.ll,"Error - XmlPullParserException");
+	    }
 	}
+	else {
+	    Log.d("AddressLookup","ll is null - ignoring");
+	    ar.onAddressFound(this.ll,"Error - ll is null - ignoring");
+	}
+    }
 }
--- WhereAreYou/src/uk/org/maps3/SMSReceiver.java
+++ WhereAreYou/src/uk/org/maps3/SMSReceiver.java
@@ -21,158 +21,161 @@ import android.widget.Toast;
 
 /**
  * @author Graham Jones
- * Class to respond to SMS messages.  The action taken depends on the contents of the SMS message.
- * If it contains 'WAYN', the current location is determined, and an SMS reply sent with details
+ * Class to respond to SMS messages.  The action taken depends on the 
+ * contents of the SMS message.
+ * If it contains 'WAYN', the current location is determined, 
+ * and an SMS reply sent with details
  * of the current locations.
- * If it contains 'WAY_UPLOAD', the SMS text is parsed assuming it is of the format:
+ * If it contains 'WAY_UPLOAD', the SMS text is parsed assuming it is 
+ * of the format:
  *   WAY_UPLOAD?UNAME=xxx&PASSWD=xxx&LON=xxx&LAT=xxx&DATE=xxx.
- * Provided the string parses correctly, a data point is uploaded to the server based on the
- * data provided.
+ * Provided the string parses correctly, a data point is uploaded to 
+ * the server based on the data provided.
  */
 public class SMSReceiver extends BroadcastReceiver 
-	implements LocationReceiver, AddressReceiver {
-
-	/**
-	 * 
-	 */
-	boolean mActive;
-	String mPassword;
-	LocationFinder lf = null;
-	Context mContext = null;
-	String smsNumber = null;
-	int mTimeOutSec = 60;
-	boolean mUseGPS = true;
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see android.content.BroadcastReceiver#onReceive(android.content.Context,
-	 * android.content.Intent)
-	 */
-	@Override
+    implements LocationReceiver, AddressReceiver {
+    
+    /**
+     * 
+     */
+    boolean mActive;
+    String mPassword;
+    LocationFinder lf = null;
+    Context mContext = null;
+    String smsNumber = null;
+    int mTimeOutSec = 60;
+    boolean mUseGPS = true;
+    
+    /*
+     * (non-Javadoc)
+     * 
+     * @see android.content.BroadcastReceiver#onReceive(android.content.Context,
+     * android.content.Intent)
+     */
+    @Override
 	public void onReceive(Context contextArg, Intent intentArg) {
-		//---get the SMS message passed in---
-		Bundle bundle = intentArg.getExtras();        
-		SmsMessage[] msgs = null;
-		mContext = contextArg;
-		getPrefs(contextArg);
-		if (bundle == null) {
-			Toast.makeText(contextArg,
-					"Empty SMS Message Received - Ignoring",
-					Toast.LENGTH_SHORT).show();
-		} else {
-			if (mActive) {
-				//---retrieve the SMS message received---
-				Object[] pdus = (Object[]) bundle.get("pdus");
-				msgs = new SmsMessage[pdus.length];            
-				for (int i=0; i<msgs.length; i++){
-					String str = "";
-					msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);                
-					str += "SMS from " + msgs[i].getOriginatingAddress();                     
-					str += " :";
-					str += msgs[i].getMessageBody().toString();
-					str += "\n";        
-					Toast.makeText(contextArg,
-							str,
-							Toast.LENGTH_SHORT).show();
-				}
-				String msg0 = msgs[0].getMessageBody().toString();
-				if (msg0.toUpperCase().contains("WAYN")) {
-					// Start the LocationFinder service if it is not running.
-					if (lf==null) {
-						lf = new LocationFinder(contextArg);
-					}
-					Toast.makeText(contextArg,
-							"Found WAYN - getting Location....",
-							Toast.LENGTH_SHORT).show();
-					// Get the location using the LocationFinder.
-					smsNumber = msgs[0].getOriginatingAddress();
-					lf.getLocationLL((LocationReceiver) this,mTimeOutSec,mUseGPS);
-					Toast.makeText(contextArg,
-							"Returned from getLocationLL()",
-							Toast.LENGTH_SHORT).show();
-				} else if  (msg0.toUpperCase().contains("WAY_UPLOAD")) {
-					Toast.makeText(contextArg,
-							"Found WAY_UPLOAD - parseing message....",
-							Toast.LENGTH_SHORT).show();
-					String msgParts[] = msg0.split("\\?");
-					if (msgParts.length>1) {
-							Toast.makeText(contextArg,"Got here",Toast.LENGTH_SHORT).show();
-						String argList[] = msgParts[1].split("\\&");
-				        List<NameValuePair> paramList = new ArrayList<NameValuePair>(2);
-						for(int i =0; i < argList.length ; i++) {
-							String key = argList[i].split("=")[0];
-							String value = argList[i].split("=")[1];
-					        paramList.add(new BasicNameValuePair(key, value));
-					        LocationUploader lu = new LocationUploader();
-					        lu.doUpload(paramList);
-							Toast.makeText(contextArg,
-									key+"="+value,
-									Toast.LENGTH_SHORT).show();
-						}
-					} else {
-						Toast.makeText(contextArg,
-								"Did not find '?' to signify start of arguments",
-									Toast.LENGTH_SHORT).show();
-					}
-					
-				} else {
-					Toast.makeText(contextArg,
-							"Message does not contain 'WAYN' or 'WAY_UPLOAD' - ignoring",
-							Toast.LENGTH_SHORT).show();
-				}
-		} else {
-				Toast.makeText(contextArg,
-						"WAYN Disabled - Ignoring",
-						Toast.LENGTH_SHORT).show();
+	//---get the SMS message passed in---
+	Bundle bundle = intentArg.getExtras();        
+	SmsMessage[] msgs = null;
+	mContext = contextArg;
+	getPrefs(contextArg);
+	if (bundle == null) {
+	    Toast.makeText(contextArg,
+			   "Empty SMS Message Received - Ignoring",
+			   Toast.LENGTH_SHORT).show();
+	} else {
+	    if (mActive) {
+		//---retrieve the SMS message received---
+		Object[] pdus = (Object[]) bundle.get("pdus");
+		msgs = new SmsMessage[pdus.length];            
+		for (int i=0; i<msgs.length; i++){
+		    String str = "";
+		    msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);                
+		    str += "SMS from " + msgs[i].getOriginatingAddress();                     
+		    str += " :";
+		    str += msgs[i].getMessageBody().toString();
+		    str += "\n";        
+		    Toast.makeText(contextArg,
+				   str,
+				   Toast.LENGTH_SHORT).show();
+		}
+		String msg0 = msgs[0].getMessageBody().toString();
+		if (msg0.toUpperCase().contains("WAYN")) {
+		    // Start the LocationFinder service if it is not running.
+		    if (lf==null) {
+			lf = new LocationFinder(contextArg);
+		    }
+		    Toast.makeText(contextArg,
+				   "Found WAYN - getting Location....",
+				   Toast.LENGTH_SHORT).show();
+		    // Get the location using the LocationFinder.
+		    smsNumber = msgs[0].getOriginatingAddress();
+		    lf.getLocationLL((LocationReceiver) this,mTimeOutSec,mUseGPS);
+		    Toast.makeText(contextArg,
+				   "Returned from getLocationLL()",
+				   Toast.LENGTH_SHORT).show();
+		} else if  (msg0.toUpperCase().contains("WAY_UPLOAD")) {
+		    Toast.makeText(contextArg,
+				   "Found WAY_UPLOAD - parseing message....",
+				   Toast.LENGTH_SHORT).show();
+		    String msgParts[] = msg0.split("\\?");
+		    if (msgParts.length>1) {
+			Toast.makeText(contextArg,"Got here",Toast.LENGTH_SHORT).show();
+			String argList[] = msgParts[1].split("\\&");
+			List<NameValuePair> paramList = new ArrayList<NameValuePair>(2);
+			for(int i =0; i < argList.length ; i++) {
+			    String key = argList[i].split("=")[0];
+			    String value = argList[i].split("=")[1];
+			    paramList.add(new BasicNameValuePair(key, value));
+			    LocationUploader lu = new LocationUploader();
+			    lu.doUpload(paramList);
+			    Toast.makeText(contextArg,
+					   key+"="+value,
+					   Toast.LENGTH_SHORT).show();
 			}
-		}
-	}
-
-	private void getPrefs(Context context) {
-		SharedPreferences settings = context.getSharedPreferences(
-				"WhereAreYou", Context.MODE_PRIVATE);
-		mActive = settings.getBoolean("Active", true);
-		mPassword = settings.getString("Password", "WAYN");
-		mTimeOutSec = settings.getInt("TimeOutSec", 60);
-		mUseGPS = settings.getBoolean("UseGPS",true);
-	}
-
-	public void onAddressLookedup(String resultStr) {
-
-	}
-
-	public void onLocationFound(LonLat ll) {
-		Toast.makeText(mContext, "onLocationFound()", Toast.LENGTH_SHORT)
-				.show();
-		if (ll != null) {
-			AddressLookup al = new AddressLookup(this);
-			al.doLookup(ll);
+		    } else {
+			Toast.makeText(contextArg,
+				       "Did not find '?' to signify start of arguments",
+				       Toast.LENGTH_SHORT).show();
+		    }
+		    
 		} else {
-			Toast.makeText(mContext, "Failed to find location - sorry!",
-					Toast.LENGTH_SHORT).show();
-			Log.d("SMSReceiver", "Failed to find location - ll is null");
+		    Toast.makeText(contextArg,
+				   "Message does not contain 'WAYN' or 'WAY_UPLOAD' - ignoring",
+				   Toast.LENGTH_SHORT).show();
 		}
-
+	    } else {
+		Toast.makeText(contextArg,
+			       "WAYN Disabled - Ignoring",
+			       Toast.LENGTH_SHORT).show();
+	    }
 	}
-
-	public void onAddressFound(LonLat ll, String addressStr) {
-		String resultStr = addressStr + "\n" + ll.toStr();
-		Log.d("SMSReceiver", "resultStr=" + resultStr);
-
-		// ---display the new SMS message on the screen.---
-		Log.d("onReceive", ll.toString());
-		Toast.makeText(mContext, " Replying: " + resultStr,
-				Toast.LENGTH_SHORT).show();
-		// Now reply to the message with our location.
-		SmsManager sm = SmsManager.getDefault();
-		sm.sendTextMessage(smsNumber, null, resultStr, null, null);
-		
+    }
+    
+    private void getPrefs(Context context) {
+	SharedPreferences settings = context.getSharedPreferences(
+								  "WhereAreYou", Context.MODE_PRIVATE);
+	mActive = settings.getBoolean("Active", true);
+	mPassword = settings.getString("Password", "WAYN");
+	mTimeOutSec = settings.getInt("TimeOutSec", 60);
+	mUseGPS = settings.getBoolean("UseGPS",true);
+    }
+    
+    public void onAddressLookedup(String resultStr) {
+	
+    }
+    
+    public void onLocationFound(LonLat ll) {
+	Toast.makeText(mContext, "onLocationFound()", Toast.LENGTH_SHORT)
+	    .show();
+	if (ll != null) {
+	    AddressLookup al = new AddressLookup(this);
+	    al.doLookup(ll);
+	} else {
+	    Toast.makeText(mContext, "Failed to find location - sorry!",
+			   Toast.LENGTH_SHORT).show();
+	    Log.d("SMSReceiver", "Failed to find location - ll is null");
 	}
 	
-	// Callback for debugging info from LocationFinder
-	public void msgBox(String msg) {
-		// TODO Do nothing - we operate silently...
-		
-	}
+    }
+    
+    public void onAddressFound(LonLat ll, String addressStr) {
+	String resultStr = addressStr + "\n" + ll.toStr();
+	Log.d("SMSReceiver", "resultStr=" + resultStr);
+	
+	// ---display the new SMS message on the screen.---
+	Log.d("onReceive", ll.toString());
+	Toast.makeText(mContext, " Replying: " + resultStr,
+		       Toast.LENGTH_SHORT).show();
+	// Now reply to the message with our location.
+	SmsManager sm = SmsManager.getDefault();
+	sm.sendTextMessage(smsNumber, null, resultStr, null, null);
+	
+    }
+    
+    // Callback for debugging info from LocationFinder
+    public void msgBox(String msg) {
+	// TODO Do nothing - we operate silently...
+	
+    }
 }
